f4ed0e5e791dc9a527c93f861e6174f189254141
ash9211 <ashishchaudhary9211@gmail.com>
2015-12-14 01:13:28 -0500

    with comments and output

--- tree.cpp
+++ tree.cpp
@@ -9,22 +9,10 @@
 **       						PATH ORAM IMPLEMENTATION						  **
 **       											written by Ashish/Anrin		  **
 **       															  **
-** This is the implementation of the PathORAM protocol along with recursive tree formation as described in the paper "https://eprint.iacr.org/2013/280.pdf"        												       **
-**Here is the list of structs and functions that have been used in the code and one/two line explaining what they do              **
-**       															  **
-**       															  **
-**       															  **
-**       															  **
-**       															  **
-**       															  **
-**       															  **
-**       															  **
-**       															  **
-**       															  **
-**       															  **
-**       															  **
-**       															  **
-**       															  **
+** This is the implementation of the PathORAM protocol along with recursive tree formation as described in the paper              ** ** "https://eprint.** iacr.org/2013/280.pdf"        									       **
+** USAGE : (1) g++ -g -O3 -std=c++11 tree.cpp                 									  **
+**    	   (2) ./a.out 														  **
+**  Description of what all the functions and structs do have been explained in two or three sentences just above their           ** **  declarations or definitions just for the explanation. For any other queries related to the code below please contact          ** **  Ashish Chaudhary at ashishchaudhary9211@gmail.com  	      	                                                            ** 
 **       															  **
 **       															  **
 **       															  **
@@ -49,21 +37,19 @@
 
 
 using namespace std;
-unordered_map<int,int> leaf_map;
-unordered_map<int,int> position_map;
-unordered_map<int,int> recleaf_map;
-unordered_map<int,int> recposition_map;
-int leaf_count = 0;
-int recleaf_count = 0;
-float numblocks;
-int data_value = 0 ;
-int global_check = 0;
-int count = 0;
-vector<pair <int,int> > stash;
-//vector<tnode> root_to_leaf_path_storage;
+unordered_map<int,int> leaf_map;        //unordered map which stores the information of all the leaf nodes of the tree
+unordered_map<int,int> position_map;    // this is the poaition map containing the mapping of blocks with the leaf nodes as given in                                             paper
+unordered_map<int,int> recleaf_map;     // analogous  of leaf_map but for recursive trees
+unordered_map<int,int> recposition_map; // analogous of position_map but for recursive trees
+int leaf_count = 0;                     // global variable that keeps the count fo the leafs in a tree
+int recleaf_count = 0;                  // analogous of leaf_count for recursive trees
+float numblocks;                        // This is the number of total blocks that were first enetered by the user 
+int global_check = 0;                   // this is a global variable which is used just to check when path is found of root to leaf						 to return...
+int count = 0;				//just a check to see if the recursive tree function is called for the first time or not so                                           that which position map has to be used is clear... for first call position_map is used and f                                         or other recposition_map is used 
+vector<pair <int,int> > stash;           // this is the stash that has been talked about in the paper... it contains the information                                            of the extra mappings of the blocks o leaf nodes which was left unfilled in the tree.
 
 
-
+// This is the struct which is made for the node of the recursive tree, this contains the mapping of the previous tree's nodes mapping to its leaf nodes in the form of a 2D matrix... This is made in this so that it can be changed for future use... The value 4 is hard coded as in the paper it was mentioned that the most optimal is to have a size of Z = 4 to be stored in the node of the tree...
 struct array_for_node{
 	int array_node[4][2] = { {-1,-1}, {-1,-1}, {-1,-1}, {-1,-1} };
 };
@@ -134,15 +120,6 @@ tnode *insert(int array[],int start, int end){
 		return NULL;
 	int mid = ( (end + start) /2);//  + start;
 	tnode *root = newnode(array[mid]);
-	/*
-	int p =0;
-	while ( p <= 3 ){
-		root->arr[p] = data_value++;
-		if (data_value   == numblocks)
-			break;
-		p++;
-	}
-	*/
 	root->left = insert(array,start,mid-1);
 	root->right = insert(array,mid+1,end);
 
@@ -217,13 +194,13 @@ void store_path(tnode *node,vector<tnode*> root_to_leaf_path_storage,int value){
 		}
 	}
 	else{
-		if (global_check == 1)
+		if (global_check == 1)  // if path to leaf found just return...
 			return;
 		store_path(node->left,root_to_leaf_path_storage,value);
-		if (global_check == 1)
+		if (global_check == 1) // if path to leaf found just return...
 			return;
 		store_path(node->right,root_to_leaf_path_storage,value);
-		if (global_check == 1)
+		if (global_check == 1) // if path to leaf found just return...
 			return;
 
 	}
@@ -231,7 +208,7 @@ void store_path(tnode *node,vector<tnode*> root_to_leaf_path_storage,int value){
 
 }
 
-// This is the basic LCA function which is used by
+// This is the basic LCA function which is used by read and write functions while inserting the blocks back into the path according to the ACCESS protocol mentioned in the paper, they should be inserted at the lca or above that in the path from root to leaf... More information in the paper which describes the access protocol... 
 tnode* lowest_common_ancestor(tnode* node, int value1, int value2){
 
 	// This is the iterative version ::  h being the height O(h) space is saved in this iterative version as no recursion stack is used.
@@ -267,7 +244,7 @@ tnode* lowest_common_ancestor(tnode* node, int value1, int value2){
 }
 
 /* Filling the initial randomness of all the blocks in the position map  */
-
+// This function maps all the blocks to the leaf nodes of the tree... for the randomness rand() function is used and all the blocks are mapped to leaf nodes present in the leaf_map... this mapping is stored in the position_map...
 void remap_all_blocks(){
 
 	srand (time(NULL)) ; //initialize the random seed
@@ -284,7 +261,7 @@ void remap_all_blocks(){
 
 }
 
-
+// This is the function which is called after remaping the blocks...This is the function according to which all the blocks are inserted in the tree's node according to the mapping of the blocks the blocks with respective leaf nodes... the insertion is done greddely from leaf to root and the extra blocks that were not able to find place in the tree nodes are kept in the stash which is a vector which is used as explained in the paper... 
 void proper_insert(tnode* node){
 	
 	cout<<"***********************************************************************************"<<endl;
@@ -297,16 +274,13 @@ void proper_insert(tnode* node){
 	
 
 
-	int check = 0;
 	for(int i =0 ; i < position_map.size(); i++){
 		//cout<<"HAHAHAHAHAAHAHAHAHAHAHAHAHHHAHAHAHAHAHA"<<endl;
 		global_check = 0;
 		path_from_root_to_leaf(node,position_map[i]);
-		check = 0;
 		int stash_size = leaf_storage_for_stash.size();
 		cout<<"for i = "<<i<<"  mapped to  = "<<position_map[i]<<endl;
-		//for (int ii= 0 ;ii<stash_size ; ii++)
-		//	cout<<leaf_storage_for_stash[ii]->data<<endl;
+		//here the insertion is happening greedily from leaf to root and if no space to enter then it is pushed into stash... 
 		while (stash_size != 0){
 		if(leaf_storage_for_stash[stash_size - 1]->arr[0] == -1){
 			leaf_storage_for_stash[stash_size - 1]->arr[0] = i;
@@ -330,7 +304,7 @@ void proper_insert(tnode* node){
 			}
 		
 		else
-			stash_size -= 1;
+			stash_size -= 1; // ensuring we are going from leaf to root...
 			}
 		if(stash_size == 0){
 			cout<<"+++++++++++++++++++++++++Pushing in stash+++++++++++++++++++++++++++++++++++++"<<endl;
@@ -351,7 +325,7 @@ void proper_insert(tnode* node){
 
 }
 
-
+// This is the read function of the access protocol, here a block is provided by the user to be read, this function is exactly what is being asked and done in the paper.... 
 void read(int block){
 	
 	cout<<"In READ Function"<<endl;
@@ -367,6 +341,7 @@ void read(int block){
 	if (position_map.find(block) == position_map.end()){
 		cout<<"block not in position map"<<endl;
 	}
+	// just getting the new mapping of the block and the old one...
 	else{
 		cout<<" ==="<<block<<"==="<<position_map[block];
 		leaf_value = position_map[block];
@@ -379,7 +354,7 @@ void read(int block){
 	for (int q = 0 ; q < numblocks; q++)
 		cout<<"  "<<q<<"     "<<position_map[q]<<endl;
 	cout<<"----------------------------------------------------------------------------------------------------------"<<endl;
-
+// path from the root to leaf is being calculated and stored in the stash 
 	path_from_root_to_leaf(root,leaf_value);
 	cout<<"Old position leaf map  "<<leaf_value<<endl;
 	cout<<"New position leaf map  "<<new_leaf_value<<endl;
@@ -393,7 +368,7 @@ void read(int block){
 	}
 	cout<<"=============================================="<<endl;	
 
-
+// here the data contained in the nodes along the path from root to leaf of the initial mapping is being stored in the stash...
 	for(int n = 0 ; n < root_to_leaf_path_storage_for_testing.size();n++){
 		//cout<<root_to_leaf_path_storage_for_testing[n]->data<<endl;
 		for(int kk = 0; kk < 4 ; kk++){
@@ -417,7 +392,7 @@ void read(int block){
 	cout<<"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
 	cout<<"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
 	cout<<"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
-
+// stash is being shuffeled to provide randomness...
 	for(int k = 0 ; k < stash.size(); k++){
 		int r = k + rand() % (stash.size() - k);
 		swap(stash[k].first, stash[r].first);
@@ -427,11 +402,11 @@ void read(int block){
 	for(int b=0 ; b<stash.size();b++)
 		cout<<stash[b].first<<"   "<<stash[b].second<<endl;
 
-
+// now the writing back of the stash to the tree is being done...
 	for(int k = stash.size() -1 ; k >= 0; k-- ){
 		int size_of_pathstorage = root_to_leaf_path_storage_for_testing.size();	
 		lca = lowest_common_ancestor(root,root_to_leaf_path_storage_for_testing[size_of_pathstorage- 1]->data,stash[k].second);
-		//cout<<lca->data<<endl;
+		// lca is being calculated and as mentioned in the paper the block is put in lca node or above in it if their is plce else it remains in the stash...
 		pos_value = 0;
 		for(int n = 0 ; n < size_of_pathstorage ; n++){
 			if(lca == root_to_leaf_path_storage_for_testing[n])
@@ -490,7 +465,7 @@ void read(int block){
 
 }
 
-
+// this function is exacltly the replica of the read function, just one difference is the chage of the block value by the value entered by the user...
 void write(int block,int data){
 	cout<<"In WRITE function"<<endl;
 
@@ -538,6 +513,7 @@ void write(int block,int data){
 		cout<<root_to_leaf_path_storage_for_testing[n]->data<<endl;
 		for(int kk = 0; kk < 4 ; kk++){
 			if (root_to_leaf_path_storage_for_testing[n]->arr[kk] == block)
+			// this is the only difference from the read function here the data of the block is being replaced by the input teh user provides...
 				root_to_leaf_path_storage_for_testing[n]->arr[kk] = data;	
 			}
 		cout<<endl;
@@ -647,7 +623,7 @@ void write(int block,int data){
 
 
 }
-
+// this function stores the information of the leaf nodes of the recursive trees...
 void recleaf_nodes(rectnode *root){
 
 	if (root == NULL)
@@ -662,7 +638,7 @@ void recleaf_nodes(rectnode *root){
 	recleaf_nodes(root->right);
 
 }
-
+// this function is the insertion of the mapping of the nodes to leaf nodes in the recursive tree nodes ... 
 rectnode *recinsert(int array[],int start, int end,int precount){
 
 
@@ -670,7 +646,7 @@ rectnode *recinsert(int array[],int start, int end,int precount){
 		return NULL;
 	int mid = ( (end + start) /2);//  + start;
 	rectnode *recroot = recnewnode(array[mid]);
-
+// if precount is -1 the it sees that this is the first recursive tree and position_map of the main tree is used 
 	if (precount == -1){
 
 		for (int i =0 ; i<4  ; i++){
@@ -680,6 +656,7 @@ rectnode *recinsert(int array[],int start, int end,int precount){
 
 		}
 	}
+// if precount is not -1 then recposition_map is used as it is building a tree for a recursive position_map...
 	else{
 		for (int i =0 ; i<4  ; i++){
 			int j  = 0;
@@ -705,7 +682,7 @@ rectnode *recinsert(int array[],int start, int end,int precount){
 	return recroot;
 
 }
-
+// this function just prints the preorder of the recursive trees 
 void recpreOrder(rectnode* node)
 {
 	if (node == NULL)
@@ -719,21 +696,8 @@ void recpreOrder(rectnode* node)
 	recpreOrder(node->left);
 	recpreOrder(node->right);
 }
-void normalpreOrder(rectnode* node)
-{
-	if (node == NULL)
-		return;
 
-	cout<<"\t"<<node->data<<endl;
-	/*for (int i =0; i <4 ;i++){
-	  int j =0;
-	  cout<<"\t    "<<node->nodearray.array_node[i][j]<<"    "<< node->nodearray.array_node[i][j+1]<<endl;
-	  }*/
-	recpreOrder(node->left);
-	recpreOrder(node->right);
-}
-
-//int count = 0;
+//This is the main function for creating the recursive trees ...
 void recursive_tree(rectnode* node, int map_size){
 
 	
@@ -751,15 +715,14 @@ void recursive_tree(rectnode* node, int map_size){
 	cout<<recposition_map.size()<<endl;
 	count++;
 
-	//normalpreOrder(node);
 	recleaf_count = 0;
 	recleaf_nodes(node);
 	cout<<recleaf_map.size()<<endl;
 	cout<<"Printing the leaf nodes : "<<endl;
 	for (int i =0 ; i < recleaf_map.size() ; i++)
 		cout<<"i = "<<i<<"  "<<recleaf_map[i]<<endl;
-
-
+	
+// for the first iteration position_map is used....
 	if (count == 1){
 
 		srand (time(NULL)) ; //initialize the random seed
@@ -768,6 +731,7 @@ void recursive_tree(rectnode* node, int map_size){
 				recposition_map.insert({q,recleaf_map[rand() % recleaf_map.size()]});
 		}
 	}
+// for all the other iterations of the recursive tree size of the recposition_map is used......
 	else{
 		srand (time(NULL)) ; //initialize the random seed
 		for(int q = 0; q < int(ceil(float(map_size)/4)); q++){
@@ -796,6 +760,7 @@ void recursive_tree(rectnode* node, int map_size){
 	cout<<"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
 	cout<<"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"<<endl;
 	int tmp = recposition_map.size();
+// clearing all the maps storing the mapping of nodes to leafs and map containg the leaf nodes information...
 	recposition_map.clear();
 	recleaf_map.clear();
 	recursive_tree(recroot,tmp);	
@@ -811,8 +776,9 @@ int main()
 	cout<<"Enter the number of blocks"<<endl;
 	cin>>numblocks;
 
+	// In the next few lines we just calculate what size the tree will be by calculating the number of nodes needed to stor ethe numblocks entered by the user and the calculate the height of the BST ...
 	int number_of_nodes_in_tree,number_of_leaf_nodes,nodes_in_com_bal_bt,diff_nodes;
-	number_of_nodes_in_tree = int(ceil(float(numblocks)/4));
+	number_of_nodes_in_tree = int(ceil(float(numblocks)/4)); // the number is hardcoded as 4 because suggested in the paper...
 	height = int(ceil(float(log2 (number_of_nodes_in_tree + 1)) - 1));
 	nodes_in_com_bal_bt = pow(2,height+1) -1;
 	diff_nodes = nodes_in_com_bal_bt - number_of_nodes_in_tree;
@@ -825,7 +791,7 @@ int main()
 	int number_arr[number_of_nodes_in_tree];
 	for(int i =0; i < number_of_nodes_in_tree;i++)
 		number_arr[i] = i+1;
-
+	// inserting thenodes in the tree, we use the method of converting array into BST...
 	root = insert(number_arr,0,number_of_nodes_in_tree-1);
 	cout<<"Printing the tree in Preorder "<<endl;
 	
@@ -841,12 +807,6 @@ int main()
 	
 	proper_insert(root);
 	preOrder(root);
-	//int node_val = 7;
-	//path_from_root_to_leaf(root,node_val);
-
-	//lowest_common_ancestor(root,val1,val2);
-	//lowest_common_ancestor(root,2,6);
-
 
 	int option,blllll,daaattt,wrrrrr;
 	cout<<"Choose one of the the oprions"<<endl;
@@ -887,11 +847,12 @@ int main()
 	}
 
 	cout<<"=============================RECURSIVE PART STARTS ==========================================================="<<endl;
-
+	// in the first recursive tree position_map is used and in all other recusrsive trees recposition_map will be used....
+	// again as suggested by papaer the value is kept to 4 and is hard coded but it can be changed as and when required...
 	int rec_tree_arr[int(ceil(float(position_map.size())/4))];
 	for(int i = 0; i < int(ceil(float(position_map.size())/4)); i++)
 		rec_tree_arr[i] = i+1 ;
-
+	// here the same method of converting the array into tree is used ...
 	recroot = recinsert(rec_tree_arr,0,int(ceil(float(position_map.size())/4)) -1, -1);
 	cout<<"Printing the recursive tree in Preorder "<<endl;
 	recpreOrder(recroot);
@@ -900,7 +861,8 @@ int main()
 	
 
 	cout<<"Printing the address for  all the roots of all the recursive trees"<<endl;
-	cout<<root<<endl;
+	cout<<root<<endl; // this is the root of the main tree......
+	//root_of_recursive_trees vector stores the root of all teh recursive trees....
 	for (int l =0 ; l <root_of_recursive_trees.size() ; l++)
 		cout<<root_of_recursive_trees[l]<<endl;
 
